10ce225503dd595c06c587fdd8721db592798ec3
Iacopo Buffoni <itsmadpupp@outlook.it>
2023-02-03 16:01:01 +0100

    Finito

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<script>
+import AppTitle from './components/AppTitle.vue';
+import AppHero from './components/AppHero.vue';
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      
+    }
+  },
+  components: {
+    AppTitle,
+    AppHero
+  }
+}
+</script>
+
+<template>
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+  </div>
+  <AppTitle msg="Vite + Vue" />
+  <AppHero/>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ src/components/AppHero.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+
+}
+</script>
+
+<template>
+    <img src="https://segwitz.com/wp-content/uploads/2021/06/vuejs-development-malaysia.jpeg" alt="">
+</template>
+
+<style scoped>
+img {
+    width: 100%;
+}
+</style>
